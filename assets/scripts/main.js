c0461fb88a31ddfdddf48c4bdcc3473a994dca15
Jason <nicholsonj863@gmail.com>
2017-05-31 20:54:33 -0400

    In the middle of trying to implement the change temperature from F to C when clicking the button. Click event was easy, now need to work out how to change the temp variable and place it in web page again.
    Issue:
    weatherArr has local scope.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,6 @@
 /**
  * Created by jason on 4/9/17.
  */
-//TODO find seven images to use with the weather changes API
 var weatherLineObj = {
     sunny: "Cloud Nine", //800
     snow: "As white as snow", //600 - 622
@@ -11,11 +10,22 @@ var weatherLineObj = {
     thunderstorm: "Storm in a teacup", // 200 - 232
     cold: "Cold enough to freeze the balls off a brass monkey" // 903
 };
+var change = 0;
+var weatherArr = [];
 
 $(document).ready(function () {
     init();
 });
 
+$(".degree").on("click", function () {
+    if (change === 0) {
+        $(".degreeWording").html("&#8451");
+        change = 1;
+    } else {
+        $(".degreeWording").html("&#8457");
+        change = 0;
+    }
+});
 
 
 function init () {
@@ -39,44 +49,44 @@ function grabWeather (lat, long){
 }
 
 function insertWeatherData (params){
-    $(".city").append("<p>" + params.city + "</p>");
-    $(".temperature").append("<p>" + params.temp + "</p>");
+    $(".city").append("<span>" + params.city + "</span>");
+    $(".temperature").prepend("<span>" + params.temp + "</span>");
     switch (true){
         //ice
         case params.weatherID === 611:
         case params.weatherID === 612:
         case params.weatherID === 906:
-            $(".description").append( "<p>" + weatherLineObj.ice + "</p>");
+            $(".description").append("<span>" + weatherLineObj.ice + "</span>");
             insertIcon(params.weatherIcon);
             break;
         //tornado
         case params.weatherID === 900:
         case params.weatherID === 961:
-            $(".description").append( "<p>" + weatherLineObj.tornado + "</p>");
+            $(".description").append("<span>" + weatherLineObj.tornado + "</span>");
             insertIcon(params.weatherIcon);
             break;
         //cold
         case params.weatherID === 903:
-            $(".description").append( "<p>" + weatherLineObj.cold + "</p>");
+            $(".description").append("<span>" + weatherLineObj.cold + "</span>");
             break;
         //sunny
         case params.weatherID >= 800:
-            $(".description").append( "<p>" + weatherLineObj.sunny + "</p>");
+            $(".description").append("<span>" + weatherLineObj.sunny + "</span>");
             insertIcon(params.weatherIcon);
             break;
         //snow
         case params.weatherID >= 600:
-            $(".description").append( "<p>" + weatherLineObj.snow + "</p>");
+            $(".description").append("<span>" + weatherLineObj.snow + "</span>");
             insertIcon(params.weatherIcon);
             break;
         //rain
         case params.weatherID >= 300:
-            $(".description").append( "<p>" + weatherLineObj.rain + "</p>");
+            $(".description").append("<span>" + weatherLineObj.rain + "</span>");
             insertIcon(params.weatherIcon);
             break;
         //thunderstorm
         case params.weatherID >= 200:
-            $(".description").append( "<p>" + weatherLineObj.thunderstorm + "</p>");
+            $(".description").append("<span>" + weatherLineObj.thunderstorm + "</span>");
             insertIcon(params.weatherIcon);
             break;
     }
@@ -85,6 +95,12 @@ function insertWeatherData (params){
 function insertIcon(iconID) {
     $(".icon").append("<img src = http://openweathermap.org/img/w/" + iconID + ".png " + ">");
 }
+
+function changeTemp() {
+
+}
+
+
 // API for weather call by zip code
 //api.openweathermap.org/data/2.5/weather?zip={45140},{USA}
 //example JSON object returned
